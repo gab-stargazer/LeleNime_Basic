428f2d834e5cbbf95899186640fa159f75046480
Insannur Kamil Malik <85838849+Kamil-Malik@users.noreply.github.com>
2022-12-22 14:23:29 +0700

    Delete feature-anime directory

--- feature-anime/src/main/java/com/lelestacia/lelenimexml/feature_anime/domain/model/Character.kt
+++
@@ -1,9 +0,0 @@
-package com.lelestacia.lelenimexml.feature_anime.domain.model
-
-data class Character(
-    val characterMalId: Int,
-    val images: String,
-    val name: String,
-    val role: String,
-    val favoriteBy: Int
-)
