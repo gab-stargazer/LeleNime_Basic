30235a90bf16ceea5dd627d1df01f3958f8fb058
kamil <km8003296@gmail.com>
2023-01-05 06:24:09 +0700

    Added verification test for Data Module

---
+++ core/data/src/test/java/com/lelestacia/lelenimexml/core/data/dummy/Chainsaw Man.kt
@@ -0,0 +1,26 @@
+package com.lelestacia.lelenimexml.core.data.dummy
+
+import com.lelestacia.lelenimexml.core.model.database.AnimeEntity
+import java.util.Date
+
+val chainsawManEntity = AnimeEntity(
+    malId = 44511,
+    coverImages = "https://cdn.myanimelist.net/images/anime/1806/126216l.jpg",
+    trailer = null,
+    title = "Chainsaw Man",
+    titleEnglish = "Chainsaw Man",
+    titleJapanese = "チェンソーマン",
+    type = "TV",
+    episodes = 12,
+    status = "Finished Airing",
+    rating = "R - 17+ (violence & profanity)",
+    score = 8.72,
+    scoredBy = 363500,
+    rank = 45,
+    synopsis = "Denji is robbed of a normal teenage life, left with nothing but his deadbeat father's overwhelming debt. His only companion is his pet, the chainsaw devil Pochita, with whom he slays devils for money that inevitably ends up in the yakuza's pockets. All Denji can do is dream of a good, simple life: one with delicious food and a beautiful girlfriend by his side. But an act of greedy betrayal by the yakuza leads to Denji's brutal, untimely death, crushing all hope of him ever achieving happiness.\n\nRemarkably, an old contract allows Pochita to merge with the deceased Denji and bestow devil powers on him, changing him into a hybrid able to transform his body parts into chainsaws. Because Denji's new abilities pose a significant risk to society, the Public Safety Bureau's elite devil hunter Makima takes him in, letting him live as long as he obeys her command. Guided by the promise of a content life alongside an attractive woman, Denji devotes everything and fights with all his might to make his naive dreams a reality.\n\n[Written by MAL Rewrite]",
+    season = "fall",
+    year = 2022,
+    genres = listOf(),
+    isFavorite = false,
+    lastViewed = Date()
+)
