27315c607e40b2428faa4559a2ee10929ea4a4ad
Insannur Kamil Malik <85838849+Kamil-Malik@users.noreply.github.com>
2023-01-05 07:20:53 +0700

    Added verification test for Data Module (#22)

---
+++ core/data/src/test/java/com/lelestacia/lelenimexml/core/data/AnimeRepositoryTest.kt
@@ -0,0 +1,126 @@
+package com.lelestacia.lelenimexml.core.data
+
+import android.content.Context
+import com.lelestacia.lelenimexml.core.data.dummy.chainsawManEntity
+import com.lelestacia.lelenimexml.core.database.ILocalDataSource
+import com.lelestacia.lelenimexml.core.network.INetworkDataSource
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit4.MockKRule
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
+import org.junit.Assert.assertThrows
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.BlockJUnit4ClassRunner
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@RunWith(BlockJUnit4ClassRunner::class)
+class AnimeRepositoryTest {
+
+    @get:Rule
+    val mockRule = MockKRule(this)
+
+    @MockK
+    lateinit var localDataSource: ILocalDataSource
+
+    @MockK
+    lateinit var networkDataSource: INetworkDataSource
+
+    @MockK
+    lateinit var mContext: Context
+
+    private lateinit var animeRepository: IAnimeRepository
+    private lateinit var characterRepository: ICharacterRepository
+
+    @Before
+    fun setup() {
+        MockKAnnotations.init(this)
+        animeRepository = AnimeRepository(networkDataSource, localDataSource, mContext)
+        characterRepository =
+            CharacterRepository(networkDataSource, localDataSource, Dispatchers.Main)
+    }
+
+    @Test
+    fun `Anime ID 0 should throw NPE`() = runTest {
+        val wrongAnimeID = 0
+        coEvery { localDataSource.getNewestAnimeDataByAnimeId(animeID = wrongAnimeID) } throws NullPointerException()
+        assertThrows(
+            NullPointerException::class.java
+        ) {
+            animeRepository.getNewestAnimeDataByAnimeId(animeID = wrongAnimeID)
+        }
+        coVerify { localDataSource.getNewestAnimeDataByAnimeId(animeID = wrongAnimeID) }
+    }
+
+    @Test
+    fun `Anime ID 44511 should return Chainsaw-Man Anime Newest Data`() = runTest {
+        val chainsawManID = chainsawManEntity.malId
+        val newestData = flowOf(
+            chainsawManEntity
+        )
+        coEvery { localDataSource.getNewestAnimeDataByAnimeId(chainsawManID) } answers { newestData }
+        val result = animeRepository.getNewestAnimeDataByAnimeId(animeID = chainsawManID)
+        result.collectLatest {
+            assertEquals(
+                "Fetching anime ID 44511 should return newest Data of Chainsaw-Man",
+                chainsawManEntity,
+                it
+            )
+            coVerify { localDataSource.getNewestAnimeDataByAnimeId(animeID = chainsawManID) }
+        }
+    }
+
+    @Test
+    fun `Anime ID 0 should return null`() = runTest {
+        val wrongAnimeID = 0
+        coEvery { localDataSource.getAnimeByAnimeId(animeID = wrongAnimeID) } answers { null }
+        val result = animeRepository.getAnimeByAnimeId(animeID = wrongAnimeID)
+        coVerify { localDataSource.getAnimeByAnimeId(animeID = wrongAnimeID) }
+        assertEquals("Fetching Anime ID 0 should return Null", null, result)
+    }
+
+    @Test
+    fun `Anime ID 44511 should return Chainsaw-Man Anime`() = runTest {
+        val chainsawManID = chainsawManEntity.malId
+        coEvery { localDataSource.getAnimeByAnimeId(animeID = chainsawManID) } answers { chainsawManEntity }
+        val result = animeRepository.getAnimeByAnimeId(animeID = chainsawManID)
+        coVerify { localDataSource.getAnimeByAnimeId(animeID = chainsawManID) }
+        assertEquals(
+            "Fetching Anime with ID 44511 should return Chainsaw Man", chainsawManEntity, result
+        )
+    }
+
+    @Test
+    fun `Verify if anime was inserted`() = runTest {
+        coEvery { localDataSource.insertOrUpdateAnime(chainsawManEntity) } answers { }
+        animeRepository.insertAnimeToHistory(chainsawManEntity)
+        coVerify { localDataSource.insertOrUpdateAnime(chainsawManEntity) }
+    }
+
+    @Test
+    fun `Verify if anime was updated properly`() = runTest {
+        val beforeUpdate = chainsawManEntity
+        val afterUpdate = beforeUpdate.copy(
+            isFavorite = !chainsawManEntity.isFavorite
+        )
+        assertNotEquals(
+            "Favorite should be different",
+            beforeUpdate.isFavorite,
+            afterUpdate.isFavorite
+        )
+        coEvery { localDataSource.updateAnime(afterUpdate) } answers { }
+        coEvery { localDataSource.getAnimeByAnimeId(chainsawManEntity.malId) } answers { beforeUpdate }
+        animeRepository.updateAnimeFavorite(chainsawManEntity.malId)
+        coVerify { localDataSource.updateAnime(afterUpdate) }
+    }
+}
---
+++ core/data/src/test/java/com/lelestacia/lelenimexml/core/data/dummy/Season Fall.kt
@@ -0,0 +1,53 @@
+package com.lelestacia.lelenimexml.core.data.dummy
+
+import com.lelestacia.lelenimexml.core.model.network.anime.NetworkAnime
+
+val chainsawManNetwork = NetworkAnime(
+    malId = 44511,
+    images = NetworkAnime.Images(
+        webp = NetworkAnime.Images.Webp(
+            imageUrl = "https://cdn.myanimelist.net/images/anime/1806/126216l.jpg",
+            largeImageUrl = "https://cdn.myanimelist.net/images/anime/1806/126216l.jpg"
+        )
+    ),
+    trailer = null,
+    title = "Chainsaw Man",
+    titleEnglish = "Chainsaw Man",
+    titleJapanese = "チェンソーマン",
+    type = "TV",
+    episodes = 12,
+    status = "Finished Airing",
+    rating = "R - 17+ (violence & profanity)",
+    score = 8.72,
+    scoredBy = 363500,
+    rank = 45,
+    synopsis = "Denji is robbed of a normal teenage life, left with nothing but his deadbeat father's overwhelming debt. His only companion is his pet, the chainsaw devil Pochita, with whom he slays devils for money that inevitably ends up in the yakuza's pockets. All Denji can do is dream of a good, simple life: one with delicious food and a beautiful girlfriend by his side. But an act of greedy betrayal by the yakuza leads to Denji's brutal, untimely death, crushing all hope of him ever achieving happiness.\n\nRemarkably, an old contract allows Pochita to merge with the deceased Denji and bestow devil powers on him, changing him into a hybrid able to transform his body parts into chainsaws. Because Denji's new abilities pose a significant risk to society, the Public Safety Bureau's elite devil hunter Makima takes him in, letting him live as long as he obeys her command. Guided by the promise of a content life alongside an attractive woman, Denji devotes everything and fights with all his might to make his naive dreams a reality.\n\n[Written by MAL Rewrite]",
+    season = "fall",
+    year = 2022,
+    genres = listOf()
+)
+
+val mobPsychoNetwork = NetworkAnime(
+    malId = 50172,
+    images = NetworkAnime.Images(
+        webp = NetworkAnime.Images.Webp(
+            imageUrl = "https://cdn.myanimelist.net/images/anime/1228/125011.jpg",
+            largeImageUrl = "https://cdn.myanimelist.net/images/anime/1228/125011.jpg"
+        )
+    ),
+    trailer = null,
+    title = "Mob Psycho 100 III",
+    titleEnglish = "Mob Psycho 100 III",
+    titleJapanese = "モブサイコ100 III",
+    type = "TV",
+    episodes = 12,
+    status = "Finished Airing",
+    rating = "PG-13 - Teens 13 or older",
+    score = 8.76,
+    scoredBy = 143325,
+    rank = 36,
+    synopsis = "After foiling a world-threatening plot, Shigeo \"Mob\" Kageyama returns to tackle the more exhausting aspects of his mundane life—starting with filling out his school's nerve-racking career form. Meanwhile, he continues to assist his mentor Arataka Reigen and the office's new recruit, Katsuya Serizawa, in solving paranormal cases of their clients. While continuing his duties, Mob also works on gaining more independence in his esper and human lives, as well as trying to integrate better with the people around him.\n\nHowever, new supernatural and ordinary challenges test Mob’s emotional stability and force him to confront the realities around him. As he strives to continue forward on the path to maturity, Mob must resolve his emotional crises and reassess the naivety he has held on for so long.\n\n[Written by MAL Rewrite]",
+    season = "fall",
+    year = 2022,
+    genres = listOf()
+)
