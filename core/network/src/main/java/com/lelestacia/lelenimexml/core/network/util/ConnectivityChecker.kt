04f4fbbde823574020d8935faa4d7e1414e4c36a
kamil <km8003296@gmail.com>
2023-01-23 18:53:11 +0700

    Added Connectivity Checker

---
+++ core/network/src/main/java/com/lelestacia/lelenimexml/core/network/util/ConnectivityChecker.kt
@@ -0,0 +1,30 @@
+package com.lelestacia.lelenimexml.core.network.util
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkCapabilities.*
+import android.os.Build
+
+class ConnectivityChecker {
+    operator fun invoke(mContext: Context): Boolean {
+        val connectivityManager: ConnectivityManager = mContext
+            .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            val network: Network = connectivityManager
+                .activeNetwork ?: return false
+            val networkCapabilities: NetworkCapabilities = connectivityManager
+                .getNetworkCapabilities(network) ?: return false
+            return when {
+                networkCapabilities.hasTransport(TRANSPORT_WIFI) -> true
+                networkCapabilities.hasTransport(TRANSPORT_CELLULAR) -> true
+                networkCapabilities.hasTransport(TRANSPORT_BLUETOOTH) -> true
+                networkCapabilities.hasTransport(TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            return connectivityManager.activeNetworkInfo?.isConnected ?: false
+        }
+    }
+}
